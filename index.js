7053cb0394341d0f615a704e18418c5a2fb27cde
RichardGrac <richard_grac@hotmail.com>
2020-01-07 14:15:33 -0600

    CORS Headers modified

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(cors())
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*")
-    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept")
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
     next()
 })
 app.set('port', process.env.PORT || 5000)
