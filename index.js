e8fa7f1c02c53c0f971439b379592154ec3474f3
RichardGrac <richard_grac@hotmail.com>
2020-01-10 14:13:53 -0600

    Adding cors for options req

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ app.use('/uploads/images', express.static(path.join('uploads', 'images')))
 
 app.use(express.json())
 app.use(cors())
+app.options('*', cors())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*")
-    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Origin', "*")
+    res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
     next()
 })
 
