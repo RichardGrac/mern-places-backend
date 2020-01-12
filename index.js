12271c524410397a6ebb144711ea130a1eda6bdd
Ricardo Garcia <Richard_Grac@hotmail.com>
2020-01-12 12:30:21 -0600

    Adding more Headers

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(bodyParser.json())
 app.use('/uploads/images', express.static(path.join('uploads', 'images')))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', "*")
-    res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization, Referer, Sec-Fetch-Mode, Sec-Fetch-Site, User-Agent, Connection, Accept-Encoding, Accept-Language")
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
     next()
 })
 
